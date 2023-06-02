8227a77b0389e4aeb6c3ab8f9922b7584df0cda5
elif-admin <elifmalkoc55@gmail.com>
2023-06-02 21:29:03 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

---
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -0,0 +1,21 @@
+PATH="$PATH:/usr/local/bin"
+APP_REPO_NAME="clarusway-repo/petclinic-app-dev"
+AWS_REGION="us-east-1"
+
+aws ecr describe-repositories --region ${AWS_REGION} --repository-name ${APP_REPO_NAME} || \
+aws ecr create-repository \
+--repository-name ${APP_REPO_NAME} \
+--image-scanning-configuration scanOnPush=false \
+--image-tag-mutability MUTABLE \
+--region ${AWS_REGION}
+```
+
+* Commit the change, then push the script to the remote repo.
+
+``` bash
+git add .
+git commit -m 'added script for creating ECR registry for dev'
+git push --set-upstream origin feature/msp-14
+git checkout dev
+git merge feature/msp-14
+git push origin dev
