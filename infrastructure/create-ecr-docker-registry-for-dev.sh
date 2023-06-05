e1b275af5a640d9b096fb9f70c710a4159bbd556
elif-admin <elifmalkoc55@gmail.com>
2023-06-05 17:35:28 +0000

    added ansible static inventory host.ini for testing

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -9,13 +9,3 @@ aws ecr create-repository \
 --image-tag-mutability MUTABLE \
 --region ${AWS_REGION}
 ```
-
-* Commit the change, then push the script to the remote repo.
-
-``` bash
-git add .
-git commit -m 'added script for creating ECR registry for dev'
-git push --set-upstream origin feature/msp-14
-git checkout dev
-git merge feature/msp-14
-git push origin dev
